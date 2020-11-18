03f5143c1b77035c6849cf49874a5fbc33b606f8
Guilherme Silveira <guilherme.silveira@caelum.com.br>
2020-11-18 14:44:59 -0300

    adding dependencies

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="imagecolorizer", # Replace with your own username
-    version="0.1.0",
+    version="0.1.3",
     author="Guilherme Silveira",
     author_email="guilherme.silveira@alura.com.br",
     description="Uses InstColorization to colorize images.",
@@ -19,4 +19,12 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
+    install_requires=[
+        'cython',
+        'pyyaml==5.1',
+        'dominate==2.4.0',
+    ],
+    dependency_links=[
+        'git+https://github.com/cocodataset/cocoapi.git#subdirectory=PythonAPI',
+    ],
 )
